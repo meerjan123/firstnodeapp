3fcede20d72c6502b210b13c4fed24be2c603e7d
meerjan123 <106593477+meerjan123@users.noreply.github.com>
2022-06-06 16:59:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello Default" }');
+    res.send('{ "response": "Hello from node app" }');
 });
 
 app.get('/ready', function (req, res) {

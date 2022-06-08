22c86b7fe47787fe771ce7accf553388bbc99344
meerjan123 <106593477+meerjan123@users.noreply.github.com>
2022-06-08 10:53:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello Welcome to the First Node App with Jenkins!!" }');
+    res.send('{ "response": "Hello Welcome to the First Node App with Jenkins!" }');
 });
 
 app.get('/ready', function (req, res) {

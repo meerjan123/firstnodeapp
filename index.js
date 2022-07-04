9989e9f7be65f570498816a0795004b32de86bf9
meerjan123 <106593477+meerjan123@users.noreply.github.com>
2022-07-04 13:10:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello!! welcome to Demo.." }');
+    res.send('{ "response": "Hello!! welcome to Demo..Deployment succeed.." }');
 });
 
 app.get('/ready', function (req, res) {
